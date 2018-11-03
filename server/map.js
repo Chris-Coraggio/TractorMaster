296dd6e34353c06948af70fc3df60a8f3f07868f
chris-coraggio <ccoraggi@purdue.edu>
2018-11-03 16:18:51 -0400

    Toggles between heatmap and markers at certain zoom levels

--- server/map.js
+++ server/map.js
@@ -18,6 +18,11 @@ function initialize() {
 
 // Fill map with markers
 function populateMarkers(data) {
+    var heatmap = []
+    var markers = []
+    data.forEach(store => {
+        heatmap.push(new google.maps.LatLng(store.latitude, store.longitude))
+    })
     data.forEach(store => {
 
         // the details of the marker for the store
@@ -28,6 +33,7 @@ function populateMarkers(data) {
             details: store.phone,
             icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
         });
+        markers.push(marker)
 
         // control what gets displayed when the icon is clicked
         var content = '<h1 class="mt0">' + marker.shopname + '</h1><p>' + marker.details + '</p>';
@@ -44,4 +50,40 @@ function populateMarkers(data) {
 
         MAPAPP.markers.push(marker);
     })
+
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatmap,
+        map: map
+    });
+
+    var gradient = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+    heatmap.set('radius', heatmap.get('radius') ? null : 20);
+    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+
+    // change the visibility of the markers based on zoom level
+
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+        var zoom = map.getZoom();
+        console.log("Current zoom level: " + zoom)
+        // iterate over markers and call setVisible
+        for (i = 0; i < markers.length; i++) {
+            markers[i].setVisible(zoom > 8);
+        }
+    });
 };
